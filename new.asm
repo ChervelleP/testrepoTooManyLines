17987fecd0ec539605d61d1d9705a954c3b616e9
ChervelleP <cpierre2022@my.fit.edu>
2026-04-24 04:26:38 -0400

    Modify message and introduce compile error
    Updated message to indicate failure and added intentional compile error.

--- new.asm
+++ new.asm
@@ -1,6 +1,6 @@
 section .data
-    msg db 'Hello, World!', 0xa
-    len equ $ - msg
+    msg1 db "This file is supposed to fail.", 10
+    len1 equ $ - msg1
 
 section .text
     global _start
@@ -8,10 +8,68 @@ section .text
 _start:
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
+    mov rsi, msg1
+    mov rdx, len1
     syscall
+
+    ; intentional compile error below
+    this_is_not_a_valid_instruction
 
     mov rax, 60
     xor rdi, rdi
     syscall
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
