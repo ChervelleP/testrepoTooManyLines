02b9fe87fd6326f11f92b35bd06e3890d6c893cc
ChervelleP <cpierre2022@my.fit.edu>
2026-04-24 05:17:24 -0400

    Refactor exit handling and remove unused buffer
    Removed buffer allocation and printing loop, changed exit code.

--- methods.asm
+++ methods.asm
@@ -1,28 +1,7 @@
-section .bss
-    buffer resb 2
-
 section .text
     global _start
 
 _start:
-    mov rcx, 1
-
-loop_start:
-    mov rax, rcx
-    add rax, '0'
-    mov [buffer], al
-    mov byte [buffer+1], 10
-
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, buffer
-    mov rdx, 2
-    syscall
-
-    inc rcx
-    cmp rcx, 4
-    jne loop_start
-
     mov rax, 60
-    xor rdi, rdi
+    mov rdi, 5   ; exit code 5
     syscall
