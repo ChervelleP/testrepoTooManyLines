40f7d0cabf9fecea6bac86c9848548601a2d0de1
ChervelleP <cpierre2022@my.fit.edu>
2026-04-24 05:16:42 -0400

    Replace Hello World message with two new lines

--- methods.asm
+++ methods.asm
@@ -1,6 +1,9 @@
 section .data
-    msg db "Hello, World!", 10
-    len equ $ - msg
+    msg1 db "First line", 10
+    len1 equ $ - msg1
+
+    msg2 db "Second line", 10
+    len2 equ $ - msg2
 
 section .text
     global _start
@@ -8,8 +11,14 @@ section .text
 _start:
     mov rax, 1
     mov rdi, 1
-    mov rsi, msg
-    mov rdx, len
+    mov rsi, msg1
+    mov rdx, len1
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg2
+    mov rdx, len2
     syscall
 
     mov rax, 60
