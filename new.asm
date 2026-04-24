fe692c4e136b8cd10ddab68292bf83b5ecd77c26
ChervelleP <cpierre2022@my.fit.edu>
2026-04-24 05:12:59 -0400

    Fix message and remove unreachable code

--- new.asm
+++ new.asm
@@ -1,26 +1,17 @@
 section .data
-    msg db "This will fail at link stage", 10
+    msg db "This program compiles and runs!", 10
     len equ $ - msg
 
 section .text
     global _start
 
 _start:
-
-    ; print message
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
     mov rdx, len
     syscall
 
-    ; jump to label that does NOT exist
-    jmp missing_label
-
-    ; normal exit (never reached)
     mov rax, 60
     xor rdi, rdi
     syscall
-
-    ; padding to exceed 60 lines
-    nop
